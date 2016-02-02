5bb27afb2a39a32b9d661b5968edd2a6add73062
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-02 12:47:17 +0200

    printf divisors of a number

--- main.c
+++ main.c
@@ -1,32 +1,14 @@
 #include <stdio.h>
 
-int calculateSumEvenNumbersUpTo(int n) {
-    int result = 0;
-    for (int i = 1; i <= n; i++) {
-        result = result + 2 * i;
+void printDivisors(int n) {
+    for (int i = 2; i < n; i++) {
+        if (n % i == 0) {
+            printf("%d\n", i);
+        }
     }
-    return result;
 }
 
-int II_SecondcalculateSumEvenNumbersUpTo(int n) {
-    int result = ;
-    for (i = 1; i <= n * 2; i = i + 2) {
-        result = result + i
-    }
-    return result;
-}
-
-
-int calculateMultipUpTo(int n) {
-    int result = 1;
-    for (int i = 1; i <= n; i++) {
-        result = result * i;
-    }
-    return result;
-}
-
-int main () {
-    printf("\nSum up to 5 is %d", calculateSumUpTo(5));
-    printf("\nMultiplication up to 5 is %d", calculateMultipUpTo(5));
+int main() {
+    printDivisors(12);
     return 0;
 }
