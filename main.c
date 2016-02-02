1c38230bde5138457afd6a3e1f24162db4bb11fd
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-02 13:12:08 +0200

    Is a number prime.

--- main.c
+++ main.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 
-void printDivisors(int n) {
-    for (int i = 2; i < n; i++) {
-        if (n % i == 0) {
-            printf("%d\n", i);
+//0-Not prime
+//1 - prime
+// 12 (2, 3,4,...,11)
+int isPrime(int n)
+{
+    if (n == 1) {
+        return 0;
+    } else {
+        for(int i=2;i<n;i++){
+            if(n%i == 0){
+            return 0;
+            }
         }
     }
+    return 1;
 }
 
-int main() {
-    printDivisors(12);
+//12 - 2,3,4,6
+int main()
+{
+    printf("4 : %d\n",isPrime(4));
+    printf("5 : %d\n",isPrime(5));
+    printf("1 : %d\n",isPrime(1));
     return 0;
 }
