304c8e50e9c9b60d5dcfee97231bdb7e28586f0b
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-03 14:56:23 +0200

    increment

--- main.c
+++ main.c
@@ -1,12 +1,9 @@
 #include <stdio.h>
 
 int main() {
-    char g[] = {'a', 'b', 'c', '\0'};
-    printf("%s\n", g);
-    printf("g=%d\n", sizeof(g));
-    char e[] = "abc";
-    printf("e=%d\n", sizeof(e));
-    char *f = "abc";
-    printf("f=%d", sizeof(f)); //char *f = "abc" why size is 8. each element has size = 2
+    int i=5, j=5, k;
+    
+    k = i++ + j;
+    printf("%d", k);
     return 0;
 }
