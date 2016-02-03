68c9101132bfad0c7178e93c3bf83e8b14aa0fde
Georgi Toshev <georgi.toshev@gmail.com>
2016-02-03 15:02:03 +0200

    swap not working

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 
+void swap(int a, int b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
 int main() {
-    int i=5, j=5, k;
+    int i = 1, j = 2;
     
-    k = i++ + j;
-    printf("%d", k);
+    swap(i, j);
+    
+    printf("i:%d, j:%d", i, j);
+    
     return 0;
 }
